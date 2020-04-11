b4075262cf8db4e262725a3d25b301be36e31d9e
fdsdev90@gmail.com <F1990pwd>
2020-04-11 23:39:40 +0200

    added main

---
+++ covid_start_info/__main__.py
@@ -0,0 +1,12 @@
+
+from controller_covid_19 import covidController
+
+
+
+if __name__ == "__main__":
+
+    print("======= COVID ANSA WEB SCRAPER =========")
+
+
+    mainControllerCovid = covidController()
+    mainControllerCovid.downloadAnsaInfo()
