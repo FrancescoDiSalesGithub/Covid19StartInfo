f427454c4fa6d9ce2a2fafc8b369f9b44b115912
fdsdev90@gmail.com <F1990pwd>
2020-04-11 12:37:30 +0200

    Created entry point

--- covid_start_info/__init__.py
+++ covid_start_info/__init__.py
@@ -3,8 +3,12 @@ from model_covid_19 import covidModel
 from controller_covid_19 import covidController
 from view_covid_19 import covidView
 
+
+
 if __name__ == "__main__":
 
+    mainControllerCovid = covidController()
+    mainControllerCovid.downloadAnsaInfo()
 
-    pass
+
 
