97d550c05398250dd20231a31cc9e59348e8defd
fdsdev90@gmail.com <F1990pwd>
2020-04-11 16:26:26 +0200

    deleted choice

--- covid_start_info/__init__.py
+++ covid_start_info/__init__.py
@@ -6,11 +6,10 @@ from controller_covid_19 import covidController
 if __name__ == "__main__":
 
     print("======= COVID ANSA WEB SCRAPER =========")
-    print("do you want to save the results on file or print on console? (0 - file 1-console) ?")
-    choice = int(input())
+
 
     mainControllerCovid = covidController()
-    mainControllerCovid.downloadAnsaInfo(choice)
+    mainControllerCovid.downloadAnsaInfo()
 
 
 
