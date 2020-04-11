26e3a5225d37bcf11563b804dc6877aa8e0b95e0
fdsdev90@gmail.com <F1990pwd>
2020-04-12 01:55:18 +0200

    removed unused function

--- view_covid_19/__init__.py
+++ view_covid_19/__init__.py
@@ -7,12 +7,6 @@ class covidView:
     def __init__(self):
         pass
 
-    def printToConsole(self,covidModel):
-        filteredInfo = covidModel.getHrefLinks()
-
-        for i in filteredInfo:
-            print(i)
-
     def saveToFile(self,covidModel):
         fileOutput = open(covidEnumerate.covidEnum.FILENAMEOUTPUT.value,"w")
         filteredInfo = covidModel.getHrefLinks()
